dcf3e63f5691906519562e490bb5adbd7698dc86
jtreeves <jackson.t.reeves@gmail.com>
2024-06-24 00:31:07 -0400

    Add Swagger docs

---
+++ cmd/api.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"net/http"
+
+	_ "github.com/jtreeves/wordiest-api/docs" // Update this path to match your module name
+
+	"github.com/jtreeves/wordiest-api/pkg/handler"
+	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
+)
+
+// @title Echo Swagger Example API
+// @version 1.0
+// @description This is a sample server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:1234
+// @BasePath /api/v1
+
+func API() {
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.GET("/docs", echoSwagger.WrapHandler)
+
+	e.GET("/words", func(c echo.Context) error {
+		return handler.GetWord(c)
+	})
+
+	e.Logger.Fatal(e.Start(":1234"))
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/jtreeves/wordiest-api/cmd"
+)
+
+func main() {
+	cmd.API()
+}
