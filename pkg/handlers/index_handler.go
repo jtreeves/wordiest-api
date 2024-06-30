0e1c2d25494c23536ea9093a009a003652397125
jtreeves <jackson.t.reeves@gmail.com>
2024-06-30 01:16:35 -0400

    Strip swagger docs from non-api endpoints

--- pkg/handlers/index_handler.go
+++ pkg/handlers/index_handler.go
@@ -11,13 +11,6 @@ func RegisterIndexRoute(index *echo.Group) {
 	index.GET("", getIndex)
 }
 
-// @Summary Get single word
-// @Description Returns a random word from the database
-// @Tags words
-// @Accept json
-// @Produce plain
-// @Success 200	{string} string "word"
-// @Router /words [get]
 func getIndex(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
 }
